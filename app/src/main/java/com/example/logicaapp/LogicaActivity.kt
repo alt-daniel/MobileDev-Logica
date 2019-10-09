ba2b5b897aa70a6af290f4cc4e4b16ec50bee634
D.P.N.P. Altorf <daniel.altorf@hva.nl>
2019-10-09 15:48:33 +0200

    Refactors variables.

--- app/src/main/java/com/example/logicaapp/LogicaActivity.kt
+++ app/src/main/java/com/example/logicaapp/LogicaActivity.kt
@@ -2,15 +2,14 @@ package com.example.logicaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_logica.*
 
 class LogicaActivity : AppCompatActivity() {
 
-    var emptyText = "One or more fields are empty!"
-    var correct = "All answers are correct!"
-    var incorrect = "One or more answers are incorrect!"
+    val EMPTY_TEXT = "One or more fields are empty!"
+    val CORRECT_MESSAGE = "All answers are CORRECT_MESSAGE!"
+    val INCORRECT_MESSAGE = "One or more answers are incorrect!"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,18 +45,17 @@ class LogicaActivity : AppCompatActivity() {
         val txtLeftCase4 = txtLeftCase4.text.toString()
         val txtRightCase4 = txtRightCase4.text.toString()
 
-
         if (resultText1.isEmpty() || resultText2.isEmpty() || resultText3.isEmpty() || resultText4.isEmpty()) {
-            Toast.makeText(this, emptyText, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, EMPTY_TEXT, Toast.LENGTH_SHORT).show()
         } else if (getLogica(txtLeftCase1, txtRightCase1) == resultText1
             && getLogica(txtLeftCase2, txtRightCase2) == resultText2
             && getLogica(txtLeftCase3, txtRightCase3) == resultText3
             && getLogica(txtLeftCase4, txtRightCase4) == resultText4
         ) {
-            Toast.makeText(this, correct, Toast.LENGTH_SHORT)
+            Toast.makeText(this, CORRECT_MESSAGE, Toast.LENGTH_SHORT)
                 .show()
         } else {
-            Toast.makeText(this, incorrect, Toast.LENGTH_SHORT)
+            Toast.makeText(this, INCORRECT_MESSAGE, Toast.LENGTH_SHORT)
                 .show()
         }
     }
